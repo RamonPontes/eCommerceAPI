2854b238d31704acb6da66b7cda8d585fd0ca7bf
RamonPontes <rgdpontes@yahoo.com>
2024-12-07 11:55:08 -0300

    Add get all product

--- src/main/java/com/ramon/eCommerceAPI/controller/ProductController.java
+++ src/main/java/com/ramon/eCommerceAPI/controller/ProductController.java
@@ -1,4 +1,21 @@
 package com.ramon.eCommerceAPI.controller;
 
+import com.ramon.eCommerceAPI.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/products")
 public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @GetMapping
+    public ResponseEntity<?> getAllProducts() {
+        return ResponseEntity.status(200).body(productService.getAllProducts());
+    }
 }
--- src/main/java/com/ramon/eCommerceAPI/service/ProductService.java
+++ src/main/java/com/ramon/eCommerceAPI/service/ProductService.java
@@ -1,4 +1,19 @@
 package com.ramon.eCommerceAPI.service;
 
+import com.ramon.eCommerceAPI.model.Product;
+import com.ramon.eCommerceAPI.repository.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ProductService {
+
+    @Autowired
+    private ProductRepository productRepository;
+
+    public List<Product> getAllProducts() {
+        return productRepository.findAll();
+    }
 }
